3f2c230956f750786bd6a529c58defd94f86e416
Jose0106 <al221910435@gmail.com>
2021-04-17 19:18:02 -0500

    cambios de aplicacion

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import CoinsStack from './src/components/coins/CoinsStack';
+import { NavigationContainer} from '@react-navigation/native';
 
-export default function App() {
+const App: () => React$Node = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+     <NavigationContainer>
+       <CoinsStack /> 
+     </NavigationContainer>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+export default App;
